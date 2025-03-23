f6227e6d2fc8c9af099725eb5c5a9e66f15ea8c6
Laura <rw21062006@gmail.com>
2025-03-23 13:15:15 +0100

    move questions to components.ts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,7 @@
 <script lang="ts">
-    import type { QuestionDTO as QuestionType } from "$lib/types";
     import Question from "$lib/Question.svelte";
-
-    let q: QuestionType[] = [
-        {
-            id: 1,
-            text: "Wer ist eigentlich Gubi Fortnite?",
-            type: "match",
-            staticOptions: [
-                "Gubi", "Niek"
-            ],
-            movableOptions: 
-            [
-                {
-                    text: "Beats",
-                    correctMatch: "Niek"
-                },
-                {
-                    text: "Fortnite",
-                    correctMatch: "Gubi"
-                }
-            ]
-        },
-        {
-            id: 2,
-            text: "Was ist eigentlich opsec?",
-            type: "mcq",
-            answers: [
-                {
-                    text: "One Piece",
-                    correct: false
-                },
-                {
-                    text: "Operation Security",
-                    correct: true
-                },
-                {
-                    text: "Overpowered Security",
-                    correct: false
-                }
-            ]
-        }
-    ]
+    import { questions } from "$lib/components";
 </script>
 
-<Question questionDTO={q[0]} />
-<Question questionDTO={q[1]} />
+<Question questionDTO={questions[0]} />
+<Question questionDTO={questions[1]} />
