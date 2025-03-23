8fc31cbb98f79fc4e0a94dc5281a66d9493eef8f
patrick <23613354+zImPatrick@users.noreply.github.com>
2025-03-23 22:08:11 +0100

    make search use lower case & only questions scrollable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,20 +3,18 @@
     import { questions } from "$lib/components";
 
     let searchValue = $state("");
-    let filteredQuestions = $derived(questions.filter(q => `${q.id}. ${q.text}`.includes(searchValue)));
+    let filteredQuestions = $derived(questions.filter(q => `${q.id}. ${q.text.toLowerCase()}`.includes(searchValue.toLowerCase())));
 </script>
 
 <main>
     <button class="start-first" onclick={() => goto("/questions/1")}>
         start training
     </button>
-
     <hr>
-    
-    <input type="text" class="search" bind:value={searchValue} placeholder="search...">
 
-    <details open>
-        <summary>Available questions</summary>
+    <input type="text" class="search" bind:value={searchValue} placeholder="search...">
+    
+    <section class="individual-section">
         <div class="question-list">
             {#each filteredQuestions as question}
                 <a href={`/questions/${question.id}`} class="card">
@@ -24,7 +22,7 @@
                 </a>
             {/each}
         </div>
-    </details>    
+    </section>
 </main>
 
 <style>
@@ -43,29 +41,40 @@
     }
 
     .search {
-        width: calc(100% - 1em);
+        width: 100%;
         padding: 0.33em;
         margin: 0 auto;
         margin-bottom: 0.25em;
+        box-sizing: border-box;
     }
 
     .question-list {
         padding-top: 0.3em;
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
         gap: 0.25rem;
         margin: 0 auto;
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
     }
 
     .card {
+        flex: 1 1 calc(100% / 3 - 0.25rem);
         border: 1px solid #000;
         border-radius: 4px;
-        padding: 1rem;
+        padding: 0.8rem;
         display: block;
         text-decoration: none;
+        box-sizing: border-box;
+        max-width: 33%;
+        word-wrap: break-word;
     }
 
     .card:any-link {
         color: #000;
+    }
+
+    .individual-section {
+        overflow: auto;
+        height: 50vh;
     }
 </style>
