ca58182c240787702f02f6923408888f832a6da8
patrick <23613354+zImPatrick@users.noreply.github.com>
2025-03-23 22:18:02 +0100

    fix: sub-questions get saved now (#4)

--- src/routes/api/answers/[questionId]/+server.ts
+++ src/routes/api/answers/[questionId]/+server.ts
@@ -6,22 +6,24 @@ import { needAuth, withValidatedInput } from "$lib/server/apiHelper";
 import { questions } from "$lib/components";
 
 export const GET: RequestHandler = needAuth(async ({ locals, params }) => {
+	const questionId = parseFloat(params.questionId!);
+
 	return json({
-		correct: wasAnswerToQuestionCorrect(locals.userData!.uuid, parseInt(params.questionId!)),
-		choices: getChosenAnswersForQuestion(locals.userData!.uuid, parseInt(params.questionId!))
+		correct: wasAnswerToQuestionCorrect(locals.userData!.uuid, questionId),
+		choices: getChosenAnswersForQuestion(locals.userData!.uuid, questionId)
 	});
 });
 
 const validateQuestionsPayload = type("number[]");
 export const PUT: RequestHandler = withValidatedInput(validateQuestionsPayload, async ({ locals, params }, data) => {
 	let wasCorrect = false;
-	const questionId = parseInt(params.questionId!);
+	const questionId = parseFloat(params.questionId!);
 
 	// todo(f): this could be cleaner
 	if (locals.userData) {
 		// putAnswerForQuestion also inserts it into the db
-		// this is why we dont call if we're not authenticated
-		wasCorrect = putAnswerForQuestion(locals.userData!.uuid, parseInt(params.questionId!), data);
+		// this is why we dont call it if we're not authenticated
+		wasCorrect = putAnswerForQuestion(locals.userData!.uuid, questionId, data);
 	} else {
 		const question = questions.find(q => q.id == questionId);
 		if (!question) {
