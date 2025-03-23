3043044784ce9baa4d1f551d4c7dae7cc2093614
patrick <23613354+zImPatrick@users.noreply.github.com>
2025-03-23 12:45:21 +0100

    Add (GET,PUT) on /api/answers/[questionId] * so you can submit your answers to the database & also get them back

---
+++ src/lib/server/apiHelper.ts
@@ -0,0 +1,37 @@
+import { json, type RequestHandler } from "@sveltejs/kit";
+import type { Type } from "arktype";
+import { type } from "arktype";
+import type { EventHandler } from "svelte/elements";
+
+type AddParametersToFunction<F extends Function, P extends unknown[]> = F extends (...a: infer FP) => infer R ? (((...a:[...FP, ...P]) => R) & Pick<F, keyof F>): never;
+
+const needAuth = (cb: RequestHandler): RequestHandler => {
+	return (...args) => {
+		const { locals } = args[0];
+		if (!locals.userData) {
+			return json({
+				"error": "not authenticated"
+			}, { status: 401 });
+		}
+
+		return cb(...args);
+	}
+}
+
+const withValidatedInput = <T extends Type<any>, G extends RequestHandler>(typeToUse: T, cb: AddParametersToFunction<G, [data: T['infer']]>): RequestHandler => {
+	return async (...args) => {
+		const { request } = args[0];
+		
+		const data = await request.json();
+		const validationResult = typeToUse(data);
+		if (validationResult instanceof type.errors) {
+			return json({
+				"error": validationResult.summary
+			}, { status: 400 });
+		}
+
+		return await cb(...args, data);
+	}
+}
+
+export { needAuth, withValidatedInput };
