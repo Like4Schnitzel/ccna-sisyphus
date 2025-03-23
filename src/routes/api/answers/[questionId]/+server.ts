eb284812a0e87fa8b69cbcdeb8c48a6a24d07c61
patrick <23613354+zImPatrick@users.noreply.github.com>
2025-03-23 20:58:29 +0100

    fix: allow validating questions while not logged in

--- src/routes/api/answers/[questionId]/+server.ts
+++ src/routes/api/answers/[questionId]/+server.ts
@@ -1,8 +1,9 @@
-import { getChosenAnswersForQuestion, putAnswerForQuestion, wasAnswerToQuestionCorrect } from "$lib/server/db/answers";
+import { getChosenAnswersForQuestion, putAnswerForQuestion, validateQuestionCorrect, wasAnswerToQuestionCorrect } from "$lib/server/db/answers";
 import { json } from "@sveltejs/kit";
 import { type } from "arktype";
 import type { RequestHandler } from "./$types";
 import { needAuth, withValidatedInput } from "$lib/server/apiHelper";
+import { questions } from "$lib/components";
 
 export const GET: RequestHandler = needAuth(async ({ locals, params }) => {
 	return json({
@@ -12,11 +13,28 @@ export const GET: RequestHandler = needAuth(async ({ locals, params }) => {
 });
 
 const validateQuestionsPayload = type("number[]");
-export const PUT: RequestHandler = needAuth(withValidatedInput(validateQuestionsPayload, async ({ locals, params }, data) => {
-	const wasCorrect = putAnswerForQuestion(locals.userData!.uuid, parseInt(params.questionId!), data);
+export const PUT: RequestHandler = withValidatedInput(validateQuestionsPayload, async ({ locals, params }, data) => {
+	let wasCorrect = false;
+	const questionId = parseInt(params.questionId!);
 
+	// todo(f): this could be cleaner
+	if (locals.userData) {
+		// putAnswerForQuestion also inserts it into the db
+		// this is why we dont call if we're not authenticated
+		wasCorrect = putAnswerForQuestion(locals.userData!.uuid, parseInt(params.questionId!), data);
+	} else {
+		const question = questions.find(q => q.id == questionId);
+		if (!question) {
+			return json({
+				"error": "Question could not be found"
+			}, { status: 400 });
+		}
+		
+		wasCorrect = validateQuestionCorrect(question, data);
+	}
+	
 	return json({
 		correct: wasCorrect,
 		choices: data
 	});
-}));
+});
